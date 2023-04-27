2c83bacbfc404a81bd60acf686d79dece6e359cc
Takuma Kira <takuma@kirakiraworx.com>
2023-04-27 11:38:28 +0900

    Try google test

---
+++ cmake-build-debug/Google_tests/lib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/Kira/Desktop/Git/DateConverter/Google_tests/lib
+# Build directory: /Users/Kira/Desktop/Git/DateConverter/cmake-build-debug/Google_tests/lib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ cmake-build-debug/Google_tests/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/Kira/Desktop/Git/DateConverter/Google_tests/lib/googletest
+# Build directory: /Users/Kira/Desktop/Git/DateConverter/cmake-build-debug/Google_tests/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
